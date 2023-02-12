242e754264ff4200c569f3fef42877bf02ed56cf
Stefan <stefantaitano@gmail.com>
2023-02-11 22:40:48 -0700

    add routes for home route

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,18 @@
 //*Handles initial GET request for the homepage
-//*Handles POST method  request for adding a new item
+
 
 const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
+const authController = require('../controllers/auth')
+const { ensureAuth, ensureGuest } = require('../middleware/auth') 
 
 router.get('/', homeController.getIndex) //read
-
+router.get('/login', authController.getLogin)
+router.post('/login', authController.postLogin)
+router.get('/logout', authController.logout)
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
 
 
 module.exports = router
