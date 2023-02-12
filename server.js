53e13c3fe0d430d6c5088101343df38f586ca131
Stefan <stefantaitano@gmail.com>
2023-02-11 22:44:50 -0700

    add new npm and update serverjs

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const PORT = 8500;
 const mongoose = require("mongoose");
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
 
 //*Import functions/routes
 const connectDB = require("./config/database")
