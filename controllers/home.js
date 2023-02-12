7994c13fd5e6d57272be25b73dfaa576ca7a8086
Stefan <stefantaitano@gmail.com>
2023-02-11 22:36:14 -0700

    create ui for landing page

--- controllers/home.js
+++ controllers/home.js
@@ -3,26 +3,9 @@ const ItemList = require('../models/ItemList')
 module.exports = {
     getIndex : async (req, res) => {
         try {
-            const items = await
-            ItemList.find()
-            res.render("index.ejs", { itemList: items });
+            res.render("index.ejs");
         } catch (err) {
             if (err) return res.status(500).send(err);
         }
     },
-    createItem: async (req, res) => {
-        const newItem = new ItemList(
-            {
-                textinput: req.body.textinput,
-                numinput: req.body.numinput
-            });
-        try {
-            await newItem.save();
-            console.log(newItem)
-            res.redirect("/");
-        } catch (err) {
-            if (err) return res.status(500).send(err);
-            res.redirect("/");
-        }
-    }
 }
--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,7 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex) //read
-router.post('/new', homeController.createItem) //create
+
 
 
 module.exports = router
